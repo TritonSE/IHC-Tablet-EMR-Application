eacef8802c506af8a18097f504365fc1a9caddb3
David Hacker <dmhacker@protonmail.com>
2021-01-17 22:39:17 -0800

    Have setup script fail immediately if error encountered

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 cd /home/pi/ihc-emr/server && npm install
 crontab -l > .tmp_ihc_server_cron
 echo "*/10 * * * * /home/pi/ihc-emr/scripts/start.sh" >> .tmp_ihc_server_cron
